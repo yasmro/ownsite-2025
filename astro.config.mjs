4da7834503362f458c68d18ebd970ebf795a2c55
Yu Ohno <kantillo0683@gmail.com>
2025-05-06 22:39:40 +0900

    impl favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import VitePWA from "@vite-pwa/astro";
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
@@ -14,5 +14,27 @@ export default defineConfig({
       plugins: [tailwindcss()]
 	},
 
-  integrations: [react()]
+  integrations: [react(), VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Yu Ohno\'s Portfolio',
+        short_name: 'yuOhno_portfolio_2025',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),]
 });
