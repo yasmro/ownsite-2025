0abd76a0af745f107e03cc6adc9a4c9a7cc579e8
y-ohno <30785878+yasmro@users.noreply.github.com>
2025-05-08 13:24:28 +0900

    Feature/design top (#3)
    * wip
    
    * fix design

--- src/components/TopPage.tsx
+++ src/components/TopPage.tsx
@@ -1,5 +1,4 @@
 import { DotWaveCanvas } from "./canvas";
-import { WaveParticles } from "./canvas/WaveParticle/WaveParticle";
 import { Button } from "./ui/button";
 
 export default function TopPage() {
@@ -10,64 +9,15 @@ export default function TopPage() {
       </div>
       <div className="relative z-10">
         <div className="h-screen flex items-center justify-center">
-          <div className="container px-4 text-center">
-            <h1 className="text-5xl md:text-7xl font-bold mb-6">
-              ドットの波の世界へ
-            </h1>
-            <p className="text-xl md:text-2xl max-w-2xl mx-auto opacity-80">
-              スクロールして、ドットの波がどのように反応するか体験してください
-            </p>
-            <a href="/test">
-              <Button>Test</Button>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="relative z-10">
-        <div className="h-screen flex items-center justify-center">
-          <div className="container px-4 text-center">
-            <h1 className="text-5xl md:text-7xl font-bold mb-6">
-              ドットの波の世界へ
-            </h1>
-            <p className="text-xl md:text-2xl max-w-2xl mx-auto opacity-80">
-              スクロールして、ドットの波がどのように反応するか体験してください
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="relative z-10">
-        <div className="h-screen flex items-center justify-center">
-          <div className="container px-4 text-center">
-            <h1 className="text-5xl md:text-7xl font-bold mb-6">
-              ドットの波の世界へ
-            </h1>
-            <p className="text-xl md:text-2xl max-w-2xl mx-auto opacity-80">
-              スクロールして、ドットの波がどのように反応するか体験してください
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="relative z-10">
-        <div className="h-screen flex items-center justify-center">
-          <div className="container px-4 text-center">
-            <h1 className="text-5xl md:text-7xl font-bold mb-6">
-              ドットの波の世界へ
-            </h1>
-            <p className="text-xl md:text-2xl max-w-2xl mx-auto opacity-80">
-              スクロールして、ドットの波がどのように反応するか体験してください
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="relative z-10">
-        <div className="h-screen flex items-center justify-center">
-          <div className="container px-4 text-center">
-            <h1 className="text-5xl md:text-7xl font-bold mb-6">
-              ドットの波の世界へ
-            </h1>
-            <p className="text-xl md:text-2xl max-w-2xl mx-auto opacity-80">
-              スクロールして、ドットの波がどのように反応するか体験してください
-            </p>
+          <div className="mx-auto text-center">
+            <img
+              src="/public/images/kyoushindohaku2.png"
+              className="mix-blend-multiply dark:mix-blend-difference dark:invert dark:grayscale"
+              loading="lazy"
+              width={132}
+              height={437}
+              alt="Kyoshindohaku"
+            />
           </div>
         </div>
       </div>
